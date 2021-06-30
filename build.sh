f493e456c29baaf67ee6cda59d193ca751bd399b
Hagb Green <hagb_green@qq.com>
2021-06-30 19:07:00 +0800

    Update hugo and theme

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@ mkdir -p static/js
 echo mirrors_list="{
 $(cd content/mirror-wiki/ ; find . -maxdepth 1 ! -name "." -type d -printf '\t"%f": "https://mirrors.cqu.edu.cn/wiki/mirror-wiki/%f",\n')
 }" > static/js/mirrors_list.js
-./hugo
+git submodule update --init && ./hugo
 
